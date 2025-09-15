c33fe52a1ca125a1e9e335f1bcfc3cf05680201e
guts-env <sdssanol@gmail.com>
2025-09-15 17:52:53 +0800

    Add Redis password support to configuration and service
    Introduces REDIS_PASSWORD_CONFIG_KEY to the config and updates RedisService to use the password from configuration. Also updates the build script to run migrations before building.

--- src/database/redis.service.ts
+++ src/database/redis.service.ts
@@ -4,6 +4,7 @@ import Redis from 'ioredis';
 import {
   REDIS_HOST_CONFIG_KEY,
   REDIS_PORT_CONFIG_KEY,
+  REDIS_PASSWORD_CONFIG_KEY,
 } from '@/config/keys.config';
 
 @Injectable()
@@ -14,6 +15,7 @@ export class RedisService {
     this.client = new Redis({
       host: this.configService.get<string>(REDIS_HOST_CONFIG_KEY),
       port: Number(this.configService.get<string>(REDIS_PORT_CONFIG_KEY)),
+      password: this.configService.get<string>(REDIS_PASSWORD_CONFIG_KEY),
       db: 1, // BullMQ uses db 0
     });
   }
