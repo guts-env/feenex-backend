073f12e32f3c18ecf160f82f56d6acc75451c2fd
guts-env <dave.sanol@ionacommerce.com>
2026-03-09 13:52:40 +0800

    Update migration script

--- src/scripts/run-migrations.ts
+++ src/scripts/run-migrations.ts
@@ -29,20 +29,28 @@ async function migrateToLatest() {
     }),
   });
 
-  const { error, results } = await migrator.migrateToLatest();
+  let hasMore = true;
 
-  results?.forEach((it) => {
-    if (it.status === 'Success') {
-      console.log(`migration "${it.migrationName}" was executed successfully`);
-    } else if (it.status === 'Error') {
-      console.error(`failed to execute migration "${it.migrationName}"`);
+  while (hasMore) {
+    const { error, results } = await migrator.migrateUp();
+
+    results?.forEach((it) => {
+      if (it.status === 'Success') {
+        console.log(
+          `migration "${it.migrationName}" was executed successfully`,
+        );
+      } else if (it.status === 'Error') {
+        console.error(`failed to execute migration "${it.migrationName}"`);
+      }
+    });
+
+    if (error) {
+      console.error('failed to migrate');
+      console.error(error);
+      process.exit(1);
     }
-  });
 
-  if (error) {
-    console.error('failed to migrate');
-    console.error(error);
-    process.exit(1);
+    hasMore = (results?.length ?? 0) > 0;
   }
 
   await db.destroy();
